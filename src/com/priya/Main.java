18eb31afd25222e008d8de7ca659966906ea1930
Priya Kumari <priyaprasad.407gmail.com>
2022-07-15 16:57:00 +0530

    First Commit

---
+++ src/com/priya/Main.java
@@ -0,0 +1,10 @@
+package com.priya;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/priya/Song.java
@@ -0,0 +1,32 @@
+package com.priya;
+
+public class Song {
+	String title;
+	double duration;
+	public Song(String title, double duration) {
+		super();
+		this.title = title;
+		this.duration = duration;
+	}
+	public Song() {
+		
+	}
+	public String getTitle() {
+		return title;
+	}
+	public double getDuration() {
+		return duration;
+	}
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//	public void setDuration(double duration) {
+//		this.duration = duration;
+//	}
+	@Override
+	public String toString() {
+		return "Song [title=" + title + ", duration=" + duration + "]";
+	}
+	 
+
+}
